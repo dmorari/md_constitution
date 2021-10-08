5e9a39b1d98173f2ac0db1cdbf397cfae088c874
Dan Morari <dan_morari@nodomain.com>
2021-10-08 11:57:08 +0300

    More questions

--- src/quizData.js
+++ src/quizData.js
@@ -139,4 +139,234 @@ export const quizData = [
 			}
 		]
 	},
+	{
+		question: {
+			"ro": "Cetăţenii Republicii Moldova pot fi extrădaţi sau expulzaţi din ţară?",
+			"ru": "Могут ли Граждане Республики Молдова быть выданы другому государству или высланы из страны?"
+		},
+		answers: [
+			{
+				"ro": "Da",
+				"ru": "Да",
+				"correct": true
+			},
+			{
+				"ro": "Nu",
+				"ru": "Нет"
+			}
+		]
+	},
+	{
+		question: {
+			"ro": "Care din următoarele afirmații privind legile noi este corectă",
+			"ru": "Какое из утверждений касательно применения новых законов верно"
+		},
+		answers: [
+			{
+				"ro": "Nimeni nu va fi condamnat pentru acţiuni sau omisiuni care, în momentul comiterii, nu constituiau un act delictuos.",
+				"ru": "Никто не может быть осужден за действия или за бездействие, которые в момент их совершения не составляли преступления.",
+				"correct": true
+			},
+			{
+				"ro": "Legea în vigoare poate fi aplicată pentru acţiuni sau omisiuni care, în momentul comiterii, nu constituiau un act delictuos.",
+				"ru": "Действующий закон может применяться против лиц, действия или бездействия которых, в момент их совершения, не составляли преступления."
+			}
+		]
+	},
+	{
+		question: {
+			"ro": "Reţinerea pentru comiterea unui act act delictuos este nu poate depași",
+			"ru": "Срок задержания не может превышать"
+		},
+		answers: [
+			{
+				"ro": "72 ore",
+				"ru": "72 часа",
+				"correct": true
+			},
+			{
+				"ro": "48 ore",
+				"ru": "48 часов"
+			},
+			{
+				"ro": "24 Ore",
+				"ru": "24 часа"
+			}
+		]
+	},
+
+	{
+		question: {
+			"ro": "Arestarea se face în temeiul ...",
+			"ru": "Арест производится на основании ..."
+		},
+		answers: [
+			{
+				"ro": "unui mandat, emis de judecător",
+				"ru": "выданного судьей ордера",
+				"correct": true
+			},
+			{
+				"ro": "unui mandat, emis de procuror",
+				"ru": "выданного прокурором ордера"
+			},
+			{
+				"ro": "unui proces verbal emis de Inspectoratul Poliției",
+				"ru": "протокола выданного генеральным инспектором полиции"
+			}
+		]
+	},
+
+
+	{
+		question: {
+			"ro": "Arestarea se face în temeiul unui mandat, emis de judecător, pentru o durată de cel mult ...",
+			"ru": "Арест производится на основании выданного судьей ордера на срок не более ..."
+		},
+		answers: [
+			{
+				"ro": "30 zile",
+				"ru": "30 дней",
+				"correct": true
+			},
+			{
+				"ro": "60 zile",
+				"ru": "60 дней"
+			},
+			{
+				"ro": "45 zile",
+				"ru": "45 дней"
+			}
+		]
+	},
+
+
+	{
+		question: {
+			"ro": "Termenul arestării poate fi prelungit de ...",
+			"ru": "Срок ареста может быть продлен ..."
+		},
+		answers: [
+			{
+				"ro": "către judecător sau de către instanţa judecătorească",
+				"ru": "судьей или судебной инстанцией",
+				"correct": true
+			},
+			{
+				"ro": "către poliție sau către instanţa de ordine publică în cazuri cînd persoana arestată prezintă pericol pentru ordinea publică",
+				"ru": "полицией или уполномоченным органом общественного порядка в случае, когда арестованное лицо представляет угрозу общественному порядку"
+			},
+			{
+				"ro": "Toate variante de răspuns sunt corecte",
+				"ru": "Все варианты ответов верны"
+			}
+		]
+	},
+
+
+	{
+		question: {
+			"ro": "Termenul arestării poate fi prelungit cel mult pînă la ...",
+			"ru": "срок ареста может быть продлен не более чем до ..."
+		},
+		answers: [
+			{
+				"ro": "12 luni",
+				"ru": "12 месяцев",
+				"correct": true
+			},
+			{
+				"ro": "6 luni",
+				"ru": "6 месяцев"
+			},
+			{
+				"ro": "Nu există limită",
+				"ru": "Нет ограничений"
+			}
+		]
+	},
+
+
+
+	{
+		question: {
+			"ro": "Percheziţiile la domiciliu sunt permise ...",
+			"ru": "обыски по месту жительства разрешаются ..."
+		},
+		answers: [
+			{
+				"ro": "doar în timpul zilei; noapte e interzis, în afară de cazul unui delict flagrant. ",
+				"ru": "только в течение дня. Ночные обыски допускаются в случае вопиющего преступления",
+				"correct": true
+			},
+			{
+				"ro": "fără restricții",
+				"ru": "без ограничений"
+			}
+		]
+	},
+
+
+	{
+		question: {
+			"ro": "Activitatea partidelor constituite din cetăţeni străini ...",
+			"ru": "деятельность партий, состоящих из иностранных граждан ..."
+		},
+		answers: [
+			{
+				"ro": "este interzisă",
+				"ru": "запрещается",
+				"correct": true
+			},
+			{
+				"ro": "este permisă",
+				"ru": "разрешается"
+			}
+		]
+	},
+
+
+	{
+		question: {
+			"ro": "După lege ...",
+			"ru": "Согласно закону ..."
+		},
+		answers: [
+			{
+				"ro": "Copiii sînt obligaţi să aibă grijă de părinţi şi să le acorde ajutor.",
+				"ru": "Дети обязаны заботиться о родителях и помогать им.",
+				"correct": true
+			},
+			{
+				"ro": "Copiii nu sînt obligaţi să aibă grijă de părinţi şi să le acorde ajutor.",
+				"ru": "Дети не обязаны заботиться о родителях и помогать им."
+			}
+		]
+	},
 ]
+
+
+/*
+
+{
+		question: {
+			"ro": "",
+			"ru": ""
+		},
+		answers: [
+			{
+				"ro": "",
+				"ru": "",
+				"correct": true
+			},
+			{
+				"ro": "",
+				"ru": ""
+			},
+			{
+				"ro": "",
+				"ru": ""
+			}
+		]
+	},
+*/
